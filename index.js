79db775e3fc72a5385e6d69f61e1465b682fd8ec
Sudharsan1002 <sudharsanselva2003@gmail.com>
2024-11-25 19:25:40 +0530

    ADDED DOCUMENTATION

--- index.js
+++ index.js
@@ -11,11 +11,18 @@ const sessionRouter = require("./controllers/sessionscontroller");
 const paymentRouter = require("./controllers/paymentcontroller");
 const { notesRouter } = require("./controllers/notescontroller");
 
+
+//CREATING A SERVER
 const API_SERVER = express();
 
+//FUNCTION TO CREATE CONNECTION WITH DATABASE
 createDbConnection();
+
+//USING CORS TO INTEGRATION
 API_SERVER.use(cors());
 
+
+//USI9NG BODY PARSER TO PARSE INCOMING REQUEST FROM BODY
 API_SERVER.use(bodyParser.json());
 
 // INJECTING ROUTES
@@ -26,6 +33,8 @@ API_SERVER.use("/sessions", sessionRouter);
 API_SERVER.use("/payments", paymentRouter);
 API_SERVER.use("/notes", notesRouter);
 
+
+//STARTING AND LISTENING TO SERVER
 API_SERVER.listen(process.env.PORT, process.env.HOSTNAME, () => {
     console.log(`http//:${process.env.HOSTNAME}:${process.env.PORT}`)
   console.log("Server started");
